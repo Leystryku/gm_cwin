eecd2887d1567defb74efd56382a1ae3f593b425
Leystryku <Leystryku@gmail.com>
2015-06-27 20:39:20 +0200

    Update stdafx.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,11 +1,5 @@
 #pragma once
 
-
-///NODEFAULTLIB /DYNAMICBASE:NO /MANIFEST:NO /MERGE:.rdata=.text
-
-#define GAME_DLL
-#define CLIENT_DLL
-
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
 #define _CRT_SECURE_NO_WARNINGS
@@ -15,9 +9,4 @@
 
 // Windows Header Files:
 #include <windows.h>
-#include <stdio.h>
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <string>
-#include <ctime>
